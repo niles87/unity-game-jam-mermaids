000d9a246d2c11c6f940c01b2d501254b880ed26
Niles Bingham <nbing19@gmail.com>
2022-05-22 17:31:36 -0700

    added animations and sprites, updated script to use animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,23 +5,34 @@ public class PlayerController : MonoBehaviour, PlayerMap.IPlayerActions {
   private PlayerMap _actions;
   private Rigidbody2D _rb;
   private GameManager _gm;
+  private Animator _anim;
+  private SpriteRenderer _sr;
 
   [SerializeField]
   private float force;
   [SerializeField]
   private float forceMultiplier;
+  private readonly float _jumpForce = 5f;
   [SerializeField]
   private bool _isGrounded;
   private bool _isJumping;
   private bool _canClimb;
+  private bool _running;
+  private bool _climbing;
+
   [SerializeField]
   private Vector2 _direction;
   [SerializeField]
   private Vector2 _climbDirection;
+  private readonly string _jumpAnim = "Jump";
+  private readonly string _runAnim = "IsRunning";
+  private readonly string _climbAnim = "IsClimbing";
 
   private void Awake() {
     _actions = new PlayerMap();
     _rb = GetComponent<Rigidbody2D>();
+    _anim = GetComponent<Animator>();
+    _sr = GetComponent<SpriteRenderer>();
     _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     _actions.Player.SetCallbacks(this);
   }
@@ -34,7 +43,16 @@ public class PlayerController : MonoBehaviour, PlayerMap.IPlayerActions {
 
   // Update is called once per frame
   void Update() {
-
+    if (_running && _isGrounded) {
+       _anim.SetBool(_runAnim, true);
+    } else {
+       _anim.SetBool(_runAnim, false);
+    }
+    if (_climbing && _canClimb) {
+       _anim.SetBool(_climbAnim, true);
+    } else {
+       _anim.SetBool(_climbAnim, false);
+    }
   }
 
   private void FixedUpdate() {
@@ -42,18 +60,20 @@ public class PlayerController : MonoBehaviour, PlayerMap.IPlayerActions {
   }
 
   private void Movement() {
-    _rb.velocity = _direction * force;
+    _rb.AddForce(_direction * force, ForceMode2D.Force);
   }
   private void Jump() {
     if ( _isJumping && _isGrounded ) {
-      _rb.AddForce(Vector2.up * ( force * forceMultiplier ), ForceMode2D.Impulse);
+      _rb.AddForce(Vector2.up * ( _jumpForce * forceMultiplier ), ForceMode2D.Impulse);
       _isGrounded = false;
+      _anim.SetTrigger(_jumpAnim);
     }
   }
 
   private void Climb() {
     if ( _canClimb ) {
       _rb.velocity = (_climbDirection * force);
+      _climbing = true;
     }
   }
 
@@ -89,13 +109,17 @@ public class PlayerController : MonoBehaviour, PlayerMap.IPlayerActions {
 
   public void OnMovement(InputAction.CallbackContext context) {
     Vector2 _inputVector = context.ReadValue<Vector2>();
-
     if ( _inputVector.x > 0 ) {
       _direction = Vector2.right;
+      _running = true;
+      _sr.flipX = false;
     } else if ( _inputVector.x < 0 ) {
       _direction = Vector2.left;
+      _running = true;
+      _sr.flipX = true;
     } else {
       _direction = Vector2.zero;
+      _running = false;
     }
 
     this.Movement();
@@ -120,6 +144,7 @@ public class PlayerController : MonoBehaviour, PlayerMap.IPlayerActions {
       _climbDirection = Vector2.down;
     } else {
       _climbDirection = Vector2.zero;
+      _climbing = false;
     }
 
     this.Climb();
